c8efecc4f2f60a57b33a2161e1c9b9a5f8524f1d
ManiDGM <manidgm1383@gmail.com>
2024-11-17 17:10:23 +0330

    project updated

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const taskInput = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('addTaskButton');
+const taskList = document.getElementById('taskList');
+
+
+addTaskButton.addEventListener('click', function () {
+    const taskText = taskInput.value.trim();
+    if (taskText === '') return; 
+
+    const taskItem = document.createElement('li');
+    taskItem.textContent = taskText;
+
+   
+    const deleteButton = document.createElement('button');
+    const trashIcon = document.createElement('i');           //creating trash icon 
+    trashIcon.className = 'fas fa-trash';
+    deleteButton.appendChild(trashIcon);
+    deleteButton.addEventListener('click', function () {
+        taskList.removeChild(taskItem);
+    });
+
+    taskItem.appendChild(deleteButton);
+
+    taskItem.addEventListener('click', function () {    //TODO: fix this part
+        taskItem.classList.toggle('completed');
+    });
+
+    taskList.appendChild(taskItem);
+    taskInput.value = ''; 
+});
