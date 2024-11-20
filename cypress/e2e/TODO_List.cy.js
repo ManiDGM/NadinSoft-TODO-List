4058359367bb7788338b150712d7a565d460309a
ManiDGM <manidgm1383@gmail.com>
2024-11-20 11:23:45 +0330

    TODO List test updated

--- cypress/e2e/TODO_List.cy.js
+++ cypress/e2e/TODO_List.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://127.0.0.1:5500/index.html')
+describe('TODO List Functionality', () => {
+  beforeEach(() => {
+      // visit the application before each test
+      cy.visit('http://127.0.0.1:5500/index.html'); // local server
+  });
+
+  it('should add a new task to the list', () => {
+      cy.get('#taskInput').type('New task');
+      cy.get('#addTaskButton').click();
+      cy.get('#taskList').should('contain', 'New task');
+  });
+
+  it('should prevent adding an empty task', () => {
+      cy.get('#addTaskButton').click();
+      cy.get('#taskList').children().should('have.length', 0);
   })
-})
+});
