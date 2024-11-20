b8cc0005fde4ae4941ad0b197d09ecf73f7ce666
ManiDGM <manidgm1383@gmail.com>
2024-11-20 16:17:20 +0330

    TODO List test updated

--- cypress/e2e/TODO_List.cy.js
+++ cypress/e2e/TODO_List.cy.js
@@ -11,7 +11,31 @@ describe('TODO List Functionality', () => {
   });
 
   it('should prevent adding an empty task', () => {
+    cy.get('#addTaskButton').click();
+    cy.get('#taskList').children().should('have.length', 0);
+   });
+
+  it('should mark  a task as completed', () => {
+      cy.get('#taskInput').type('New task');
       cy.get('#addTaskButton').click();
-      cy.get('#taskList').children().should('have.length', 0);
-  })
+      cy.get('#taskList li button i.fa-check').click();
+   });
+
+  it('should edit a task', () => {
+      cy.get('#taskInput').type('New task');
+      cy.get('#addTaskButton').click();
+      cy.get('#taskList li button i.fa-edit').click();
+      cy.window().then((win) => {
+        cy.stub(win, 'prompt').returns('Another new task');
+    });
+      cy.get('#taskList li span').should('contain', 'Another new task');    //Note: write "Another new task" while testing the code in cypress
+   });                                                                      // TODO: fix the problem
+  
+   it('shoult delete a task', () => {
+       cy.get('#taskInput').type('New task');
+       cy.get('#addTaskButton').click();
+       cy.get('#taskList li button i.fa-trash').click();
+       cy.get('#taskList').should('not.contain', 'New task')
+   });
+
 });
