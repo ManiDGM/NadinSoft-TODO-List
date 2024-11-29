b0d522b2b83384e7b9615e341c233e09c285b50e
ManiDGM <manidgm1383@gmail.com>
2024-11-29 12:31:33 +0330

    e2e testing for ts files added to the project

---
+++ cypress/e2e/TODO_List.cy.ts
@@ -0,0 +1,40 @@
+describe('TODO List Functionality', () => {
+  beforeEach(() => {
+      // visit the application before each test
+      cy.visit('http://127.0.0.1:5500/index.html'); // local server
+  });
+
+  it('should add a new task to the list', () => {
+      cy.get('#taskInput').type('New task');
+      cy.get('#addTaskButton').click();
+      cy.get('#taskList').should('contain', 'New task');
+  });
+
+  it('should prevent adding an empty task', () => {
+    cy.get('#addTaskButton').click();
+    cy.get('#taskList').children().should('have.length', 0);
+   });
+
+  it('should mark  a task as completed', () => {
+      cy.get('#taskInput').type('New task');
+      cy.get('#addTaskButton').click();
+      cy.get('#taskList li button i.fa-check').click();
+   });
+
+  it('should edit a task', () => {
+      cy.get('#taskInput').type('New task');
+      cy.get('#addTaskButton').click();
+      cy.get('#taskList li button i.fa-edit').click();
+      cy.get('.edit-input').type('Another new task');
+      cy.get('#addTaskButton').click();
+      cy.get('#taskList li span').should('contain', 'Another new task');    
+   });                                                                      
+  
+   it('shoult delete a task', () => {
+       cy.get('#taskInput').type('New task');
+       cy.get('#addTaskButton').click();
+       cy.get('#taskList li button i.fa-trash').click();
+       cy.get('#taskList').should('not.contain', 'New task')
+   });
+
+});
